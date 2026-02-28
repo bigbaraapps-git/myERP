7a5587490e479aace5e2e9addb201f918ee07432
Zoran Ruzic <zruzic@Zorans-Mac-mini.Bigbara>
2026-02-28 15:07:57 +0100

    chore: ignore local build cache and db

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+3PCATkDAyXtl+f67CShXpB+V1MDlr4V9ZTFxGkbAxE=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_7b3adbbb._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_fc1f78e4._.js",
-  "static/chunks/src_5ce5e39c._.js"
-],
-    source: "dynamic"
-});
